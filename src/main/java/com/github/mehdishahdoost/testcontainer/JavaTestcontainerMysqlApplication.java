a236af553705911028c717867d0f6fb063166aa7
Mehdi Shahdoost <mhdshahdoost@gmail.com>
2022-12-03 10:54:29 +0100

    Init

---
+++ src/main/java/com/github/mehdishahdoost/testcontainer/JavaTestcontainerMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.mehdishahdoost.testcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestcontainerMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestcontainerMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mehdishahdoost/testcontainer/JavaTestcontainerMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mehdishahdoost.testcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestcontainerMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
